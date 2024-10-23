e6357373a71c706602b5a12e769589acf616ae28
Jackesque <tientranquang123@gmail.com>
2024-10-23 17:52:49 +0700

    update cors

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
+import { CLIENT_URL } from "./constants/index.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: CLIENT_URL,
+}));
 app.use(express.json());
 app.use("/record", records);
 
