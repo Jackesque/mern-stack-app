379fa4b076c6ff4e835b26f4020ac704a3ecdc1e
Jackesque <tientranquang123@gmail.com>
2024-10-26 09:28:47 +0700

    update url

--- mern/server/constants/index.js
+++ mern/server/constants/index.js
@@ -1,6 +1,6 @@
-// export const CLIENT_URL = "https://mern-stack-app-client.onrender.com";
-// export const SERVER_URL = "https://mern-stack-app-aj7m.onrender.com";
+export const CLIENT_URL = "https://mern-stack-app-client.onrender.com";
+export const SERVER_URL = "https://mern-stack-app-aj7m.onrender.com";
 
 // test
-export const CLIENT_URL = "http://localhost:5173";
-export const SERVER_URL = "http://localhost:5050";
+// export const CLIENT_URL = "http://localhost:5173";
+// export const SERVER_URL = "http://localhost:5050";
